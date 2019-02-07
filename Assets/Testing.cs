a02a29894a06462ec6f136b07bf78450548237a2
Calvin Min <Calvinjmin@gmail.com>
2019-02-07 08:39:53 -0500

    Initial Logging

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Testing : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+
     public float speed;
     public GameObject bullet;
+    public Text log;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        log.text = "";
     }
 
     // Update is called once per frame
@@ -20,9 +24,7 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Create a new bullet at “transform.position” 
-            // Which is the current position of the ship
-            // Quaternion.identity = add the bullet with no rotation
+            StartCoroutine(produceBullet());
             Instantiate(bullet, transform.position, Quaternion.identity);
         }
     }
@@ -33,5 +35,16 @@ public class Testing : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+    }
+
+    void WriteLog( string text ) {
+        log.text = text;
+    }
+
+    IEnumerator produceBullet()
+    {
+        WriteLog("Created Bullet");
+        yield return new WaitForSeconds(1);
+        WriteLog("");
     }
 }
