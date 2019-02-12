47649d457cfb20237d9a8c5ba70bcd4cdc256ea2
Calvin Min <Calvinjmin@gmail.com>
2019-02-12 13:51:45 -0500

    Power and Angle Adaptivity

--- Assets/Scripts/bulletScript.cs
+++ Assets/Scripts/bulletScript.cs
@@ -7,20 +7,18 @@ public class bulletScript : MonoBehaviour
 {
     // Public variable 
     public Slider powerSlider;
-    private Rigidbody2D r2d;
+    public Rigidbody2D r2d;
     private Vector2 movement;
+
 
     // Function called once when the bullet is created
     void Start()
     {
-        r2d = GetComponent<Rigidbody2D>();
-        movement = new Vector2( Mathf.Sqrt(Mathf.Pow(powerSlider.value,2)) / 10 , Mathf.Sqrt(Mathf.Pow(powerSlider.value, 2)) / 10 );
-        r2d.velocity = movement;
+
     }
 
-    void update()
+    void Update()
     {
-        r2d.AddForce(movement * 1);
     }
 
     // Function called when the object goes out of the screen
@@ -28,5 +26,10 @@ public class bulletScript : MonoBehaviour
     {
         // Destroy the bullet 
         Destroy(gameObject);
+    }
+
+    void InitialVelocity( Vector2 v )
+    {
+        r2d.AddForce(v);
     }
 }
