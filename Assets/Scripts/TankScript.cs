8c5f387e57229d194e53e9c058d3e87cd2a31b85
Calvin Min <Calvinjmin@gmail.com>
2019-02-19 13:45:25 -0500

    Power Ups - Bullet Mirage

--- Assets/Scripts/TankScript.cs
+++ Assets/Scripts/TankScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,50 +7,101 @@ using UnityEngine.UI;
 public class TankScript : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private GameObject weapon;
+    private System.Random randomNum;
     private bool playerTurn;
+    private float player1Loc = 0;
+    private float player2Loc = 0;
+    private string[] player1Logs = new string[100];
+    private string[] player2Logs = new string[100];
 
     public float speed;
     public GameObject bullet;
+    public GameObject laser;
 
     public GameObject player1;
     public GameObject player2;
 
+    public Dropdown powerUpsDropdown;
+
     public Text log;
-    public Slider powerSlider;
-    public Slider angleSlider;
+    public Slider xPowerSlider;
+    public Slider yPowerSlider;
     public Button fireButton;
+    public Button powerUps;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
         Button btn = fireButton.GetComponent<Button>();
         btn.onClick.AddListener(ShootBullet);
 
+        Button powerBtn = powerUps.GetComponent<Button>();
+        powerBtn.onClick.AddListener(PowerUpdate);
+
         log.text = "Player 1's Turn";
         playerTurn = true;
+        xPowerSlider.value = 50;
+        yPowerSlider.value = 0;
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 
-    void ShootBullet()
-    { 
-        StartCoroutine(ProduceBullet());
-        GameObject tempBullet = new GameObject();
-        if ( playerTurn)
+    void PowerUpdate() {
+        Debug.Log("Power Up is changed to " + powerUpsDropdown.options[powerUpsDropdown.value].text);
+        if (powerUpsDropdown.options[powerUpsDropdown.value].text == "Laser")
         {
-            tempBullet = Instantiate(bullet, player1.transform.position, Quaternion.identity);
+            weapon = laser;
         }
         else
         {
-            tempBullet = Instantiate(bullet, player2.transform.position, Quaternion.identity);
+            weapon = bullet;
         }
-        tempBullet.SendMessage("InitialVelocity", new Vector2(powerSlider.value * 5, angleSlider.value * 5));
+    }
 
+    void ShootBullet()
+    {
+        try
+        {
+            StartCoroutine(ProduceBullet());
+            GameObject tempBullet = new GameObject();
+            if (playerTurn)
+            {
+                if (powerUpsDropdown.options[powerUpsDropdown.value].text == "Bullet Mirage")
+                {
+                    BulletMirage(player1.transform.position);
+                }
+                else
+                {
+                    tempBullet = Instantiate(weapon, player1.transform.position, Quaternion.identity);
+                    tempBullet.SendMessage("InitialVelocity", new Vector2(xPowerSlider.value * 7, yPowerSlider.value * 7));
+                }
+            }
+            else
+            {
+                if (powerUpsDropdown.options[powerUpsDropdown.value].text == "Bullet Mirage")
+                {
+                    BulletMirage(player2.transform.position);
+                }
+                else
+                {
+                    tempBullet = Instantiate(weapon, player2.transform.position, Quaternion.identity);
+                    tempBullet.SendMessage("InitialVelocity", new Vector2(xPowerSlider.value * 7, yPowerSlider.value * 7));
+                }
+            }
+        }
+        catch ( NullReferenceException e )
+        {
+            Debug.Log("INPUT A VALID WEAPON THROUGH THE POWER UPS!");
+        }
     }
 
     void FixedUpdate()
@@ -67,18 +119,56 @@ public class TankScript : MonoBehaviour
 
     IEnumerator ProduceBullet()
     {
-        WriteLog("Bullet shot with an X-Velocity of " + powerSlider.value + "\n" + 
-                 "Bullet shot with a Y-Velocity of " + angleSlider.value);
-        yield return new WaitForSeconds(2);
         if (playerTurn)
         {
+            WriteLog( "Player 1: \n" +
+                      "Bullet shot with an X-Velocity of " + xPowerSlider.value + "\n" +
+                      "Bullet shot with a Y-Velocity of " + yPowerSlider.value);
+            yield return new WaitForSeconds(2);
+            logBackground("Player 1: Bullet shot with an X-Velocity of " + xPowerSlider.value , "Bullet shot with a Y-Velocity of " + yPowerSlider.value);
             log.text = "Player 2's Turn";
+            xPowerSlider.value = -50;
+            yPowerSlider.value = 0;
             playerTurn = false;
         }
         else
         {
+            WriteLog( "Player 2: \n" + 
+                      "Bullet shot with an X-Velocity of " + xPowerSlider.value + "\n" +
+                      "Bullet shot with a Y-Velocity of " + yPowerSlider.value);
+            logBackground(" Player 2: Bullet shot with an X-Velocity of " + xPowerSlider.value, "Bullet shot with a Y-Velocity of " + yPowerSlider.value);
+            yield return new WaitForSeconds(2);
             log.text = "Player 1's Turn";
+            xPowerSlider.value = 50;
+            yPowerSlider.value = 0;
             playerTurn = true;
         }
+    }
+
+    void logBackground( string x , string y )
+    {
+        if (playerTurn)
+        {
+            Debug.Log(x + "\n" + y);
+            player1Logs[(int)player1Loc] = x + " " + y;
+        }
+        else
+        {
+            Debug.Log(x + "\n" + y);
+            player2Logs[(int)player2Loc] = x + " " + y;
+        }
+    }
+
+    void BulletMirage( Vector2 position )
+    {
+        GameObject bulletMirage = new GameObject();
+        int temp = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                bulletMirage = Instantiate(weapon, position, Quaternion.identity);
+                temp = randomNum.Next(-20, 20);
+                bulletMirage.SendMessage("InitialVelocity", new Vector2((xPowerSlider.value + temp) * 7, (yPowerSlider.value + temp) * 7));
+            }
+        
     }
 }
