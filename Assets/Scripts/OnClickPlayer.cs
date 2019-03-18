d6bc5bed7f3d4c00c6dd8729d2b4d24f8d3c8662
Kiyan Zewer <kiyanzewer@gmail.com>
2019-03-18 14:03:38 -0400

    Character Selection Worked on with some Menu Updates

---
+++ Assets/Scripts/OnClickPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickPlayer : MonoBehaviour
+{
+    public Text player1;
+    public Text player2;
+    public Button select;
+
+    public void OnClick(string name)
+    {
+        if (OnClickSelect.getReady() == false)
+        {
+            if (OnClickSelect.currentPlayerVal() == 1)
+            {
+                player1.text = "Player 1: " + name;
+            }
+            else
+            {
+                player2.text = "Player 2: " + name;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/OnClickSelect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickSelect : MonoBehaviour
+{
+    public Text current;
+    public static int currentPlayer = 1;
+    int clickCounter = 0;
+    public Text player1;
+    public Text player2;
+    public Button select;
+    public Button menu;
+    public static bool ready = false;
+
+    public void OnClick()
+    {
+        if (player1.text.Length > 10)
+        {
+            menu.interactable = false;
+            clickCounter++;
+            current.text = "Currently: Player 2";
+            currentPlayer = 2;
+            if (player2.text.Length > 10 && clickCounter >= 2)
+            {
+                select.interactable = false;
+                ready = true;
+            }
+        }
+    }
+
+    public static int currentPlayerVal()
+    {
+        return currentPlayer;
+    }
+
+    public static bool getReady()
+    {
+        return ready;
+    }
+}
