24bfacca82f1dab48e7ad9fd29f5fe1507ee5124
Kiyan Zewer <kiyanzewer@gmail.com>
2019-03-26 13:53:32 -0400

    Made Character Selection Cleaner (Hidden Images Until Clicked)

---
+++ Assets/OnStartHideImages.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnStartHideImages : MonoBehaviour
+{
+    public Image sprite;
+
+    void Start()
+    {
+        sprite.enabled = false;
+    }
+
+    
+}
---
+++ Assets/OnStartHideText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnStartHideText : MonoBehaviour
+{
+    public Text desc;
+    void Start()
+    {
+        desc.enabled = false;
+    }
+
+    
+}
